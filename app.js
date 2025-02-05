2de98104521e8239d981453f20471830af882183
MuhammadrizoShuhratov <shuhratovmuhammadrizo24@gmail.com>
2025-02-05 23:12:30 +0500

    primitive add

---
+++ app.js
@@ -0,0 +1,46 @@
+// let a = "anvar";
+// let isStudent = true;
+// let b = 5;
+// let student = null;
+// let bigint = 2222222222222222222222222222n;
+// let id = Symbol("👌");
+// let qiymat;
+
+// console.log(typeof a);
+// console.log(typeof isStudent);
+// console.log(typeof b);
+// console.log(typeof student);
+// console.log(typeof bigint);
+// console.log(typeof id);
+// console.log(typeof qiymat);
+
+
+    // let a = "10";
+    // a = String(a);
+    // a = a + 1;
+    // console.log(a);
+    // a = Number(a);
+    // console.log(a);
+
+    // let b = "5";
+    // b = Number(b) + 12121;
+    // console.log(b);
+
+    // let c = "11";
+    // console.log(c + 1);
+
+    let x = 8;
+    console.log(x/6);
+
+
+// let Number;
+// console.log(typeof Number);
+
+// let b = "10";
+// let b = "4";
+// console.log(b + 3478787887);
+
+
+
+// var box = "2015";
+// console.log(box);
